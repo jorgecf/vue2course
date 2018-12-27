2e6fefd8c3ad3414cb6abbf2378c978132f17f62
Jorge <jcifuentes@afi.es>
2018-12-27 14:51:45 +0100

    Components

---
+++ webpack-simple-project/project/src/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <p>Home: {{ msg }}</p>
+        <button @click="changeMsg">Say goodbye!</button>
+    </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+        msg: "hello world!"   
+    }
+  },
+
+  methods: {
+      changeMsg() { // ES6 style
+            this.msg ="bye world";
+      }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
