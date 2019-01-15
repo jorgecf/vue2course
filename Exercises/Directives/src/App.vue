796dd589ed049137b70373810793073bc6a2e043
jorgecf <jorge.cifuentes95@gmail.com>
2019-01-15 15:44:30 +0100

    Started directives

---
+++ Exercises/Directives/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Built-in Directives</h1>
+
+                <p v-text="'Text binding'"></p>
+                <p v-html="'<b>Text</b> binding'"></p>
+
+            </div>
+        </div>
+
+        <hr>
+
+         <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Custom Directives</h1>
+
+                <p>This should be colored!</p>
+
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style>
+
+</style>
