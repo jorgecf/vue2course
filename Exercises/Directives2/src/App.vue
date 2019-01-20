3b03264341fcb49868823a8fe9318fccfe26ed1e
Jorge Cifuentes <Jorge.cifuentes95@gmail.com>
2019-01-20 09:32:37 +0100

    Filters

---
+++ Exercises/Directives2/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <button v-myon:click="clickHandler">Directives Exercise</button>
+                <!-- Exercise -->
+                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        directives: {
+            "myon": {
+                bind(el, binding, vnode) {
+                    el.addEventListener(binding.arg, binding.value);
+                }
+            }
+        },
+        methods: {
+            clickHandler() {
+                console.log("Clicked!");
+            }
+        }
+
+
+    }
+</script>
+
+<style>
+</style>
